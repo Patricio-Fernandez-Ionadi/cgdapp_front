a9f2570a1919afdb67c24d0c03d28f0339fac1de
Patricio Fernandez Ionadi <patricio.f.i@live.com>
2021-05-22 18:00:30 -0300

    navigation started

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,22 +1,78 @@
-import React from "react"
-
+import React, { useState } from "react"
+// STYLES
 import { makeStyles } from "@material-ui/core"
+// icons
+import MenuIcon from "@material-ui/icons/Menu"
+import CloseIcon from "@material-ui/icons/Close"
 
 const useStyles = makeStyles((theme) => ({
 	navigation: {
-		maxWidth: "200px",
-		width: "100%",
-
+		width: "200px",
+		minHeight: "100vh",
 		backgroundColor: "#ccc",
+		transition: "all .3s ease",
+		position: "fixed",
+		left: "0",
 	},
+	nav_closed: {
+		left: "-200px",
+	},
+
+	// MENU
+	menu_burguer: {
+		width: "40px",
+		height: "40px",
+		margin: "10px",
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+		border: "1px solid #db281b",
+		borderRadius: "3px",
+		transition: "all .3s ease",
+		boxShadow: "0 0 3px 1px #000 inset",
+		position: "fixed",
+		left: "0",
+		"&:hover": {
+			backgroundColor: "#db281b",
+		},
+		"& *": {
+			color: "#ffcd01",
+		},
+	},
+	menu_open: {
+		backgroundColor: "#db281b",
+		left: "200px",
+	},
+	// ----------------
 }))
+//-------------------------------
 
 const Navigation = () => {
-	const classes = useStyles()
-	const { navigation } = classes
+	// STYLES
+	const { navigation, nav_closed, menu_burguer, menu_open } = useStyles()
+	//-------------------------------
+
+	// estado de menu (MOBILE)
+	const [isOpen, setIsOpen] = useState(false)
+	const handleMenu = () => {
+		setIsOpen(!isOpen)
+	}
+	//-------------------------------
+
 	return (
-		<nav className={navigation}>
-			<p>NAVEGACION</p>
+		<nav
+			className={isOpen ? `${navigation}` : `${navigation} ${nav_closed}`}
+			onClick={handleMenu}
+		>
+			{isOpen ? (
+				<div onClick={handleMenu} className={`${menu_burguer} ${menu_open}`}>
+					<CloseIcon />
+				</div>
+			) : (
+				<div onClick={handleMenu} className={`${menu_burguer}`}>
+					<MenuIcon />
+				</div>
+			)}
 		</nav>
 	)
 }
